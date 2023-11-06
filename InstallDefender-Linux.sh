2127ca985a299abd46ed4b5d32831d1c4e1bf03b
n4t5ru <hello@nasru.me>
2023-11-06 14:05:03 +0500

    Automated all tasks in script to accomodate all OS

--- InstallDefender-Linux.sh
+++ InstallDefender-Linux.sh
@@ -3,12 +3,12 @@
 : '
     Author:         n4t5u
     Email:          hello@nasru.me
-    Version:        1.0
+    Version:        2.0
     License:        MIT
     Created:        05/11/2023
-    Modified:       -
+    Modified:       06/11/2023
     ScriptName:     InstallDefender-Linux
-    Description:    Automated the installation steps to install Windows Defender Endpoint on Debian Linux Distributions
+    Description:    Automated the installation steps to install Windows Defender Endpoint on Linux Distributions
                     Check https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/microsoft-defender-endpoint-linux?view=o365-worldwide for supported distributions
     How To:         Run the script as Root
 '
@@ -35,45 +35,97 @@ function main() {
 
     ${yum}${apt}${zypper} install curl libplist-utils gpg gnupg apt-transport-https -y > /dev/null
 
-    # Edit [Distro] [Version] [Channel]
-    # curl -o microsoft.list https://packages.microsoft.com/config/[distro]/[version]/[channel].list
-    # mv ./microsoft.list /etc/apt/sources.list.d/microsoft-[channel].list
-    curl -o microsoft.list http://packages.microsoft.com/config/ubuntu/22.04/prod.list
-    mv ./microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
+    # Checks /etc/os-release file and retrieves all the required variables for the require downloads to work
+    for file in /etc/*
+    do
+        if [[ "${file}" == "/etc/os-release" ]]
+        then
+            OsID=$(grep -w "ID" /etc/os-release)
+            OsVer=$(grep -w "VERSION_ID" /etc/os-release)
 
-    # For Debian 12
-    #curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-prod.gpg > /dev/null
+            if [[ ${OsID} == "ID=debian" ]] && [[ ${OsVer} == 'VERSION_ID="12"' ]]
+            then
+                curl -o microsoft.list http://packages.microsoft.com/config/debian/12/prod.list
+                mv ./microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
 
-    # For Other Distributions
-    curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+                # For Debian 12 this part varies.
+                curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-prod.gpg > /dev/null
+                break
+            else
+                osRemove="ID="
+                verRemove="VERSION_ID="
+
+                # Remove double quotes and the specified part from OS ID
+                osFinal="${OsID//$osRemove/}"
+                osFinal="${osFinal//\"/}"
+
+                # Remove double quotes and the specified part from OS Version
+                verFinal="${OsVer//$verRemove/}"
+                verFinal="${verFinal//\"/}"
+
+                if [[ ${osFinal} == 'debian']]
+                then
+                    curl -o microsoft.list http://packages.microsoft.com/config/${osFinal}/$verFinal/prod.list
+                elif [[ ${osFinal} == 'ubuntu' ]]
+                then
+                    curl -o microsoft.list http://packages.microsoft.com/config/${osFinal}/$verFinal/prod.list
+                else
+                    curl -o microsoft.list http://packages.microsoft.com/config/${osFinal}/$verFinal/prod.repo
+                fi
+
+                mv ./microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
+                curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+                break
+            fi
+        fi
+    done
 
     # Update the Repo Metadata
     echo "${green}This machine will update Repo Metadata."
-    sleep 10
+    sleep 5
     $normal
-    apt update -y
+    apt update -y > /dev/null
+    sleep 10
 
     # Now install the MDATP
-    apt install mdatp
+    apt install mdatp > /dev/null
 
     # For this Part to work, you will require the ZIP file to be present in the same folder as the script.
     # Microsoft Provided ZIP file Unzip it and run the Python File
     unzip WindowsDefenderATPOnboardingPackage.zip
     python3 MicrosoftDefenderATPOnboardingLinuxServer.py
+    sleep 10
 
     # Checking if Device is enrolled.
     echo "${red}Checking if Device has been enrolled."
     mdatp health --field org_id
-    sleep 10
+    sleep 5
     mdatp health --field healthy
+    sleep 5
+
+    # Checking if Realtime Threat Protection is enabled and enables it.
+    threat_Protection='mdatp health --field real_time_protection_enabled'
+
+    if [[ ${threat_Protection} == 'false' ]];
+    then
+        mdatp config real-time-protection --value enabled
+        mdatp config tamper_protection --value enabled
+        mdatp config tamper_protection --value enabled
+        echo "${green} Real-time Threat Protection Enabled."
+        sleep 10
+    else
+        echo "${yellow} Real-time Threat Protection Already Activated"
+    fi
+
+    # Exiting the script
+    echo "${normal} We done here! Bye"
     sleep 10
+    exit
 }
 
 #Checks for sudo access before running the main function
 if [[ ${UID} == 0 ]]; then
     main
-
-    exit 1
 else
     echo "${red}This script requires elevated access. Please run using SUDO or run the Script as ROOT."
     Sleep 5
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## How to:
 - Make the script an executable ```chmod +x InstallDefender-Linux.sh```
 - Run the script with ``SUDO`` permission
+
+## More Info
+- This script automates the tasks outlined in Microsoft Documentation (https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/linux-install-manually?view=o365-worldwide)
+- This script has been tested on Debian and Ubuntu
+    - Issues might arise from other OSes
+- ``WindowsDefenderATPOnboardingPackage.zip`` has to be downloaded and kept in the same folder as the script for it to work.
